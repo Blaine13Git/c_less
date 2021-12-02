705d47d06fd9dbdbb1feaa867362153f5cfa0d13
muyi <fengchang@xinc818.group>
2021-12-02 09:48:42 +0800

    调整和新增第四章

--- part_2_3/overflow_example.c
+++ part_2_3/overflow_example.c
@@ -16,7 +16,7 @@ int main(int argc, char *argv[]) {
     printf("[BEFORE] buffer_one is at %p and contains \'%s\'\n", buffer_one, buffer_one);
     printf("[BEFORE] value is at %p and is %d (0c%08x)\n", &value, value, value);
 
-    printf("\n[STRCPY] copying %d bytes into buffer_two\n\n", strlen(argv[1]));
+    printf("\n[STRCPY] copying %lu bytes into buffer_two\n\n", strlen(argv[1]));
     strcpy(buffer_two, argv[1]);
 
     printf("[AFTER] buffer_two is at %p and contains \'%s\'\n", buffer_two, buffer_two);
